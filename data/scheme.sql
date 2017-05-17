727de8c254e8dd8472172da356f03c89da4ffbf6
Your Name <you@example.com>
2017-05-17 22:23:32 +0300

    Обновлен sql-файл для создания таблиц.

--- data/scheme.sql
+++ data/scheme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 16 2017 г., 19:37
+-- Время создания: Май 17 2017 г., 21:18
 -- Версия сервера: 10.1.21-MariaDB
 -- Версия PHP: 5.6.30
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `category` (
-  `id_cat` int(10) NOT NULL,
+  `category_id` int(10) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -38,13 +38,13 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `post` (
-  `id_post` int(10) NOT NULL,
-  `title` varchar(256) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `title` varchar(256) NOT NULL DEFAULT '',
   `text` text NOT NULL,
-  `date_add` datetime NOT NULL,
-  `date_edit` datetime NOT NULL,
-  `id_user` int(10) NOT NULL,
-  `view_status` tinyint(1) NOT NULL
+  `user_id` int(10) NOT NULL,
+  `is_visible` tinyint(1) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -54,8 +54,9 @@ CREATE TABLE `post` (
 --
 
 CREATE TABLE `post_category` (
-  `id_post` int(10) NOT NULL,
-  `id_cat` int(10) NOT NULL
+  `id` int(10) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `category_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,7 +66,7 @@ CREATE TABLE `post_category` (
 --
 
 CREATE TABLE `user` (
-  `id_user` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `login` varchar(64) NOT NULL,
@@ -83,27 +84,28 @@ CREATE TABLE `user` (
 -- Индексы таблицы `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id_cat`);
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Индексы таблицы `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`id_post`),
-  ADD KEY `id_user` (`id_user`);
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `id_user` (`user_id`);
 
 --
 -- Индексы таблицы `post_category`
 --
 ALTER TABLE `post_category`
-  ADD PRIMARY KEY (`id_post`,`id_cat`),
-  ADD KEY `id_cat` (`id_cat`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -113,17 +115,22 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_cat` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `post`
 --
 ALTER TABLE `post`
-  MODIFY `id_post` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `post_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `post_category`
+--
+ALTER TABLE `post_category`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -132,14 +139,14 @@ ALTER TABLE `user`
 -- Ограничения внешнего ключа таблицы `post`
 --
 ALTER TABLE `post`
-  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Ограничения внешнего ключа таблицы `post_category`
 --
 ALTER TABLE `post_category`
-  ADD CONSTRAINT `post_category_ibfk_1` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`),
-  ADD CONSTRAINT `post_category_ibfk_2` FOREIGN KEY (`id_cat`) REFERENCES `category` (`id_cat`);
+  ADD CONSTRAINT `post_category_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+  ADD CONSTRAINT `post_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
