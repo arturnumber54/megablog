8bd606a298536574991bb2dd5104c00d4aeecd79
test <test@test.com>
2017-05-20 10:27:05 +0300

    Заданы дефолтные значения для сервиса роутинга.

--- Core/Routing/Router.php
+++ Core/Routing/Router.php
@@ -1,22 +1,27 @@
 <?php
 
+const DEFAULT_CONTROLLER = "index";
+const DEFAULT_ACTION = "index";
+
 class Router
 {
-	// Конструктор.
-	public function __construct(){
-		
-	}
-	
-	public function route(){
-		if(isset($_GET['route'])){
-			$temp = explode('/', $_GET['route']);
-			
-			$params['controller'] = $temp[0];
-			$params['action'] = $temp[1];
-			
-			return $params;
-		}
-		
-		return null;
-	}
+    // Конструктор.
+    public function __construct()
+    {
+    	
+    }
+
+    public function route()
+    {
+        if (isset($_GET['route'])) {
+            $temp = explode('/', $_GET['route']);
+            $params['controller'] = $temp[0];
+            $params['action'] = $temp[1];
+        } else {
+            $params['controller'] = DEFAULT_CONTROLLER;
+            $params['action'] = DEFAULT_ACTION;
+        }
+        
+        return $params;
+    }
 }
